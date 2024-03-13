83c796ed50206771ff7368a5b5535d4f79421534
Nhatduy13nhatduy <nhatduy13nhatduy@gmail.com>
2024-03-13 21:41:26 +0700

    Create global_variables.dart
    Update color name

---
+++ frontend/lib/constants/global_variables.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class GlobalVariables {
+  //Define Colors
+  //Color black and white has been defined
+  static const Color defaultColor = Color(0xFFFAFAFA);
+  static const Color darkGrey = Color(0xFF808089);
+  static const Color lightGrey = Color(0xFFF2F4F5);
+  static const Color darkGreen = Color(0xFF198155);
+  static const Color green = Color(0xFF23C16B);
+  static const Color lightGreen = Color(0xFFDAEFDE);
+  static const Color skinColor = Color(0xFFFCD9BB);
+  static const Color darkRed = Color(0xFFBF1D28);
+  static const Color lightRed = Color(0xFFFFDBDE);
+  static const Color darkYellow = Color(0xFFCC8100);
+  static const Color yellow = Color(0xFFFFC400);
+  static const Color lightYellow = Color(0xFFFFF5C7);
+  static const Color darkBlue = Color(0xFF0D5BB5);
+  static const Color lightBlue = Color(0xFFDBEEFF);
+}
