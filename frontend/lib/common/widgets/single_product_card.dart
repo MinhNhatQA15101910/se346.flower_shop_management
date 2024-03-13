585578e9308a373dbdfa4801ab11f72b54f3af50
Do Nhat <dmnhat.pt@gmail.com>
2024-03-13 21:42:14 +0700

    Add basic widgets for sprint 1.

---
+++ frontend/lib/common/widgets/single_product_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SingleProductCard extends StatelessWidget {
+  const SingleProductCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
