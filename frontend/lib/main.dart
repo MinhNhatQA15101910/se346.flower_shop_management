86a8aed37959a7682448a8b48c9c69c2e80118a2
KIWI\ADMIN <21522217@gm.uit.edu.vn>
2024-03-20 14:18:55 +0700

    khiem add layout auth 1/2 + test navigate

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/constants/global_variables.dart';
+import 'package:frontend/features/auth/screens/welcome_screen.dart';
+import 'package:go_router/go_router.dart';
 import 'package:frontend/features/auth/screens/intro_screen.dart';
 
 void main() {
@@ -8,27 +10,37 @@ void main() {
   );
 }
 
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return IntroScreen();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'welcomescreen',
+          builder: (BuildContext context, GoRouterState state) {
+            return const WelcomeScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    GlobalVariables.init(context);
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: 'Flowerfly',
-      theme: ThemeData(
-        scaffoldBackgroundColor: GlobalVariables.defaultColor,
-        colorScheme: const ColorScheme.light(
-          primary: GlobalVariables.green,
-        ),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: GlobalVariables.darkGreen,
-          ),
-        ),
-      ),
-      home: const IntroScreen(),
+      theme:
+          ThemeData(), // Let the theme default like this my friend, we touch it later
+      routerConfig: _router,
     );
   }
 }
