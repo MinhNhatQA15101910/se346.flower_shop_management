3a831a164a41f5d4f8e77b169e88294256807fbb
Do Nhat <dmnhat.pt@gmail.com>
2024-04-22 10:00:42 +0700

    Add occasions table to the database.

--- backend/queries.sql
+++ backend/queries.sql
@@ -34,19 +34,22 @@ CREATE TABLE types (
 );
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (1, 'All Cakes', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623093/types/All%20Cakes/types-all-cakes_zo9jdu.jpg');
+VALUES (1, 'All Combos', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624804/types/All%20Combos/types-all-combos_lhsww9.jpg');
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (1, 'Cartoon Cake', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623235/types/Cartoon%20Cake/types-cartoon-cake_u8n2c9.jpg');
+VALUES (1, 'Cakes & Flower', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624494/types/Cakes%20and%20Flower/types-cakes-and-flower_gk5kqy.jpg');
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (1, 'Floral Cake', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623316/types/Floral%20Cake/types-floral-cake_ky5mhe.jpg');
+VALUES (1, 'Flower & Chocolate', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624559/types/Flower%20and%20Chocolate/types-flower-and-chocolate_dtj1o9.jpg');
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (1, 'Heart Shaped', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623368/types/Heart%20Shaped/types-heart-shaped_xy73wx.jpg');
+VALUES (1, 'Flowers & Teddy', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624611/types/Flowers%20and%20Teddy/types-flowers-and-teddy_zsiaqj.jpg');
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (1, 'Round Shaped', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623490/types/Round%20Shaped/types-round-shaped_swhrkz.jpg');
+VALUES (1, 'Jumbo Combo', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624695/types/Jumbo%20Combo/types-jumbo-combo_qjbw6l.jpg');
+
+INSERT INTO types (category_id, name, image_url)
+VALUES (1, 'Greeting Card Combo', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624775/types/Greeting%20Card%20Combo/types-greeting-card-combo_smy5yn.jpg');
 
 INSERT INTO types (category_id, name, image_url)
 VALUES (2, 'All Flowers', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713592337/types/All%20Flowers/types-all-flowers_g8qn1f.jpg');
@@ -73,19 +76,81 @@ INSERT INTO types (category_id, name, image_url)
 VALUES (2, 'Chocolate Bouquet', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713593125/types/Chocolate%20Bouquet/types-chocolate-bouquet_awbeuh.jpg');
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (3, 'All Combos', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624804/types/All%20Combos/types-all-combos_lhsww9.jpg');
+VALUES (3, 'All Cakes', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623093/types/All%20Cakes/types-all-cakes_zo9jdu.jpg');
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (3, 'Cakes & Flower', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624494/types/Cakes%20and%20Flower/types-cakes-and-flower_gk5kqy.jpg');
+VALUES (3, 'Cartoon Cake', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623235/types/Cartoon%20Cake/types-cartoon-cake_u8n2c9.jpg');
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (3, 'Flower & Chocolate', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624559/types/Flower%20and%20Chocolate/types-flower-and-chocolate_dtj1o9.jpg');
+VALUES (3, 'Floral Cake', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623316/types/Floral%20Cake/types-floral-cake_ky5mhe.jpg');
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (3, 'Flowers & Teddy', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624611/types/Flowers%20and%20Teddy/types-flowers-and-teddy_zsiaqj.jpg');
+VALUES (3, 'Heart Shaped', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623368/types/Heart%20Shaped/types-heart-shaped_xy73wx.jpg');
 
 INSERT INTO types (category_id, name, image_url)
-VALUES (3, 'Jumbo Combo', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624695/types/Jumbo%20Combo/types-jumbo-combo_qjbw6l.jpg');
+VALUES (3, 'Round Shaped', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713623490/types/Round%20Shaped/types-round-shaped_swhrkz.jpg');
 
-INSERT INTO types (category_id, name, image_url)
-VALUES (3, 'Greeting Card Combo', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713624775/types/Greeting%20Card%20Combo/types-greeting-card-combo_smy5yn.jpg');
+CREATE TABLE occasions (
+    id SERIAL PRIMARY KEY,
+    category_id INT,
+    name VARCHAR(20) NOT NULL,
+    image_url VARCHAR(200) NOT NULL,
+    
+    CONSTRAINT fk_types_categories_category_id
+    FOREIGN KEY (category_id)
+    REFERENCES categories(id)
+);
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (1, 'Valentine''s Day', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626169/occasions/Valentine%27s%20Day/occasions-valentine-day_gdvolw.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (1, 'Birthday Combo', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626197/occasions/Birthday%20Combo/occasions-birthday-combo_ilifl5.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (1, 'Mother''s Day', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626281/occasions/Mother%27s%20Day/occasions-mother-day_gxuarc.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (1, 'Father''s Day', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626310/occasions/Father%27s%20Day/occasions-father-day_n3thih.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (1, 'Women''s Day', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626361/occasions/Women%27s%20Day/occasions-women-day_a7yakw.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (2, 'Anniversary', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626741/occasions/Anniversary/occasions-anniversary_j4orem.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (2, 'Birthday', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626786/occasions/Birthday/occasions-birthday_sjifo9.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (2, 'Valentine', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626818/occasions/Valentine/occasions-valentine_rzpefx.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (2, 'Get Well', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626856/occasions/Get%20Well/occasions-get-well_qs1g7t.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (2, 'Sorry', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626906/occasions/Sorry/occasions-sorry_m6o2it.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (2, 'Rose Day', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713626942/occasions/Rose%20Day/occasions-rose-day_um5u5c.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (3, 'Birthday Cake', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713627530/occasions/Birthday%20Cake/occasions-birthday-cake_gy0lpi.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (3, 'Anniversary Cake', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713627559/occasions/Anniversary%20Cake/occasions-anniversary-cake_budzyo.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (3, 'Engagement Cake', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713754256/occasions/Engagement%20Cake/occasions-engagement-cake_ei8fsa.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (3, 'Wedding Cake', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713754295/occasions/Wedding%20Cake/occasions-wedding-cake_n5ie4f.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (3, 'Valentine''s Day', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713754335/occasions/Valentine%27s%20Day/occasions-valentine-day_satctb.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (3, 'Mother''s Day', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713754377/occasions/Mother%27s%20Day/occasions-mother-day_k5n09z.jpg');
+
+INSERT INTO occasions (category_id, name, image_url)
+VALUES (3, 'Father''s Day', 'https://res.cloudinary.com/dauyd6npv/image/upload/v1713754416/occasions/Father%27s%20Day/occasions-father-day_y2mmyl.jpg');
--- frontend/lib/features/customer/order_management/screens/order_management_screen.dart
+++ frontend/lib/features/customer/order_management/screens/order_management_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/common/widgets/admin_drawer.dart';
+import 'package:frontend/features/admin/admin_drawer.dart';
 import 'package:frontend/features/customer/order_management/widgets/order_detail_cart.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:frontend/constants/global_variables.dart';
@@ -27,78 +27,81 @@ class _OrderManagementScreenState extends State<OrderManagementScreen> {
     return DefaultTabController(
       length: 5,
       child: Scaffold(
-          appBar: AppBar(
-            title: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      width: GlobalVariables.screenWidth * 0.6,
-                      child: TextField(
-                        controller: _textController,
-                        style: GoogleFonts.inter(
+        appBar: AppBar(
+          title: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: GlobalVariables.screenWidth * 0.6,
+                    child: TextField(
+                      controller: _textController,
+                      style: GoogleFonts.inter(
+                        color: GlobalVariables.darkGrey,
+                        fontSize: 16,
+                      ),
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.all(8),
+                        prefixIconColor: GlobalVariables.darkGrey,
+                        suffixIconColor: GlobalVariables.darkGrey,
+                        hintStyle: GoogleFonts.inter(
                           color: GlobalVariables.darkGrey,
                           fontSize: 16,
                         ),
-                        decoration: InputDecoration(
-                          contentPadding: const EdgeInsets.all(8),
-                          prefixIconColor: GlobalVariables.darkGrey,
-                          suffixIconColor: GlobalVariables.darkGrey,
-                          hintStyle: GoogleFonts.inter(
-                            color: GlobalVariables.darkGrey,
-                            fontSize: 16,
-                          ),
-                          hintText: 'Enter the product keyword to search',
-                          prefixIcon: const Icon(Icons.search),
-                          // suffixIcon: IconButton(
-                          //   enableFeedback: false,
-                          //   onPressed: () {
-                          //     _textController.clear();
-                          //   },
-                          //   icon: const Icon(Icons.clear),
-                          // ),
-                          enabledBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: GlobalVariables.lightGrey),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: GlobalVariables.darkGreen),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
+                        hintText: 'Enter the product keyword to search',
+                        prefixIcon: const Icon(Icons.search),
+                        // suffixIcon: IconButton(
+                        //   enableFeedback: false,
+                        //   onPressed: () {
+                        //     _textController.clear();
+                        //   },
+                        //   icon: const Icon(Icons.clear),
+                        // ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: GlobalVariables.lightGrey),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: GlobalVariables.darkGreen),
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
                     ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.account_circle_outlined,
-                        size: 30,
-                      ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.account_circle_outlined,
+                      size: 30,
                     ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          bottom: _buildTabbar(tabbarList),
+        ),
+        drawer: AdminDrawer(),
+        body: TabBarView(
+          children: [
+            for (int i = 0; i < tabbarList.length; i++)
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 12.0,
+                ),
+                child: Column(
+                  children: [
+                    OrderDetailCart(),
                   ],
                 ),
-              ],
-            ),
-            bottom: _buildTabbar(tabbarList),
-          ),
-          drawer: AdminDrawer(),
-          body: TabBarView(
-            children: [
-              for (final tab in tabbarList)
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 12.0),
-                  child: Column(
-                    children: [
-                      OrderDetailCart(),
-                    ],
-                  ),
-                ),
-            ],
-          )),
+              ),
+          ],
+        ),
+      ),
     );
   }
 
