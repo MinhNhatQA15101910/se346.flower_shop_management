1502f06f0c0b66df1674ebc60c30e4aafcb9304c
Do Nhat <dmnhat.pt@gmail.com>
2024-06-06 16:25:17 +0700

    Add delete from cart route

--- backend/routers/customer/cart_router.js
+++ backend/routers/customer/cart_router.js
@@ -165,4 +165,61 @@ cartRouter.delete(
   }
 );
 
+cartRouter.delete(
+  "/customer/delete-from-cart/:product_id",
+  authValidator,
+  async (req, res) => {
+    try {
+      const db = getDatabaseInstance();
+
+      const { product_id } = req.params;
+
+      db.query("DELETE FROM carts WHERE user_id = $1 AND product_id = $2", [
+        req.user,
+        product_id,
+      ]);
+
+      let user = await db.query("SELECT * FROM users WHERE id = $1", [
+        req.user,
+      ]);
+      const productsResult = await db.query(
+        "SELECT DISTINCT p.* FROM products p, carts c, users u WHERE p.id = c.product_id AND c.user_id = $1 ORDER BY p.id ASC",
+        [user.rows[0].id]
+      );
+      const products = productsResult.rows;
+      for (let i = 0; i < products.length; i++) {
+        let imageUrlList = await db.query(
+          "SELECT image_url FROM product_images WHERE product_id = $1",
+          [products[i].id]
+        );
+
+        let imageUrls = [];
+        for (let j = 0; j < imageUrlList.rowCount; j++) {
+          imageUrls.push(imageUrlList.rows[j].image_url);
+        }
+
+        products[i].image_urls = imageUrls;
+      }
+      const quantitiesResult = await db.query(
+        "SELECT quantity FROM carts WHERE user_id = $1 ORDER BY product_id ASC",
+        [user.rows[0].id]
+      );
+      let quantities = [];
+      for (let i = 0; i < quantitiesResult.rowCount; i++) {
+        quantities.push(quantitiesResult.rows[i].quantity);
+      }
+
+      await db.end();
+      res.json({
+        token: req.token,
+        ...user.rows[0],
+        products,
+        quantities,
+      });
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+);
+
 export default cartRouter;
