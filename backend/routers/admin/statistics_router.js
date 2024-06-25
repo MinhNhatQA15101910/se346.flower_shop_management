a14dbdacd9be8e59be16369cf7544832c1d19f60
Do Nhat <dmnhat.pt@gmail.com>
2024-06-25 23:04:01 +0700

    product statistics by category route.

--- backend/routers/admin/statistics_router.js
+++ backend/routers/admin/statistics_router.js
@@ -1,6 +1,8 @@
 import express from "express";
 import pg from "pg";
 
+import adminValidator from "../../middlewares/admin_validator.js";
+
 const statisticsRouter = express.Router();
 
 function getDatabaseInstance() {
@@ -14,5 +16,38 @@ function getDatabaseInstance() {
   db.connect();
   return db;
 }
+
+statisticsRouter.get(
+  "/admin/product-analytics/categories",
+  adminValidator,
+  async (req, res) => {
+    try {
+      const db = getDatabaseInstance();
+
+      const categories = await db.query("SELECT * FROM categories");
+
+      let categoryMap = [];
+      for (let i = 0; i < categories.rowCount; i++) {
+        const products = await db.query(
+          "SELECT DISTINCT p.*, c.* FROM products p, product_type pt, types t, categories c WHERE p.id = pt.product_id AND pt.type_id = t.id AND t.category_id = c.id AND c.id = $1 UNION SELECT DISTINCT p.*, c.* FROM products p, product_occasion po, occasions o, categories c WHERE p.id = po.product_id AND po.occasion_id = o.id AND o.category_id = c.id AND c.id = $1",
+          [categories.rows[i].id]
+        );
+
+        let value = 0;
+        for (let j = 0; j < products.rowCount; j++) {
+          value += Number(products.rows[j].stock);
+        }
+
+        categoryMap.push({ name: categories.rows[i].name, value });
+      }
+
+      await db.end();
+
+      res.json(categoryMap);
+    } catch (e) {
+      res.status(500).json({ error: e.message });
+    }
+  }
+);
 
 export default statisticsRouter;
--- frontend/lib/constants/global_variables.dart
+++ frontend/lib/constants/global_variables.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-String uri = 'http://192.168.111.177:3000';
+String uri = 'http://192.168.2.115:3000';
 
 class GlobalVariables {
   // Base Variables
