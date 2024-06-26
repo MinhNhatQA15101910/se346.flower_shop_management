9d6f32b9c11892cad6b1a7cb90365ac436ed6b40
Nhatduy13nhatduy <nhatduy13nhatduy@gmail.com>
2024-06-26 17:06:13 +0700

    Update

--- frontend/lib/features/admin/category_management/screens/category_management_screen.dart
+++ frontend/lib/features/admin/category_management/screens/category_management_screen.dart
@@ -20,10 +20,10 @@ class _CategoryManagementScreenState extends State<CategoryManagementScreen> {
 
   List<Category>? _categories;
 
-  void _fetchAllCategories() {
-    setState(() async {
-      _categories = await _homeService.fetchAllCategories(context);
-    });
+  void _fetchAllCategories() async {
+    _categories = await _homeService.fetchAllCategories(context);
+
+    setState(() {});
   }
 
   @override
